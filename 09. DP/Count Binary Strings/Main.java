5f030ee9717e2816178a6b9cf8943b8dcb59d335
Rohit Tyagi <92664353+MrRohitTyagi@users.noreply.github.com>
2022-03-07 19:52:06 +0530

    Add files via upload

---
+++ 09. DP/Count Binary Strings/Main.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception {
+    Scanner scn  = new Scanner(System . in);
+    int n = scn.nextInt();
+
+    int [][] dp = new int [2][n+1];
+    dp[0][1] = 1;
+    dp[1][1] = 1;
+
+    for( int i = 2  ;i<=n  ; i++ ) {
+        dp[0][i]= dp[1][i-1];
+        dp[1][i] = dp[0][i-1] +dp[1][i-1];
+    }
+    int finalAns = dp[0][n]+dp[1][n];
+    System.out.println(finalAns);
+ }
+
+}
