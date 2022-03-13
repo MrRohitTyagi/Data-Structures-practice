654090a316dd9351f138e413f3d088e95e29b789
Rohit Tyagi <92664353+MrRohitTyagi@users.noreply.github.com>
2022-03-13 16:46:46 +0530

    Add files via upload

---
+++ Stacks/Duplicate Brackets/Main.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    public static boolean check(String s){
+        Stack<Character> st = new Stack<>();
+        
+        for(int i = 0 ;i < s.length() ;i++ ){
+            
+           char ch = s.charAt(i);
+           if(ch != ')'){
+               st.push(ch);
+           }
+           else{
+               if(st.peek() == '('){
+                   return true;
+               }
+               while(st.peek() != '(' ){
+                   st.pop();
+               }
+               st.pop();
+           }
+            
+        }
+        return false;
+        
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner (System.in);
+        String s = scn.nextLine();
+        System.out.println(check(s));
+        
+        
+       
+    }
+}
