88a06c302884e0dccac08e2663db59f9c600be52
Rohit Tyagi <92664353+MrRohitTyagi@users.noreply.github.com>
2022-04-03 04:40:19 +0530

    Add files via upload

---
+++ Binary Search Tree/GFG Minimum element in BST/Main.java
@@ -0,0 +1,13 @@
+class Tree {
+    // Function to find the minimum element in the given BST.
+    int minValue(Node node) {
+        if(node  == null){
+            return - 1;
+        }
+        Node curr = node;
+        while(curr.left !=  null){
+            curr = curr.left;
+        }
+        return curr.data;
+    }
+}
